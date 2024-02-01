ca62b9a3ecb4135d097e911a228587b2a3012af6
MustafaOzer20 <ozerstt@hotmail.com>
2024-02-01 20:17:05 +0300

    limit max value added

--- django_gui/gui/forms.py
+++ django_gui/gui/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class DriverFilterForm(forms.Form):
-    startDate = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)
-    endDate = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)
-    minScore = forms.FloatField(required=False)
-    maxScore = forms.FloatField(required=False)
-    limit = forms.IntegerField(min_value=1, required=False)
-    offset = forms.IntegerField(min_value=0, required=False)
+    startDate = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=True)
+    endDate = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=True)
+    minScore = forms.FloatField(required=True)
+    maxScore = forms.FloatField(required=True)
+    limit = forms.IntegerField(min_value=1, max_value=150, required=True)
+    offset = forms.IntegerField(min_value=0, required=True)
